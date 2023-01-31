6555e6a1815b17c81a38baa6f4018f5784c2fbc7
Alexandre Vessoni <av.vessoni@gmail.com>
2023-01-31 07:37:48 +0000

    Created room + redirect to room'

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,41 @@
+import { get, getDatabase, ref } from 'firebase/database';
+import { FormEvent, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import googleIconImg from '../assets/images/google-icon.svg';
 import logoImg from '../assets/images/logo.svg';
 import { Button } from '../components/Button';
 import { useAuth } from '../hooks/useAuth';
+import { app } from '../services/firebase';
 
 export function Home() {
+  const [roomCode, setRoomCode] = useState('');
   const navigate = useNavigate();
 
   const { user, signInwithGoogle } = useAuth();
+
   const handleCreateRoom = async () => {
     if (!user) {
       await signInwithGoogle();
     }
     navigate('/newRoom');
   };
+
+  async function handleJoinRoom(event: FormEvent) {
+    event.preventDefault();
+    if (roomCode.trim() === '') {
+      return;
+    }
+
+    const db = getDatabase(app);
+    const roomRef = await get(ref(db, `rooms/${roomCode}`));
+    if (!roomRef.exists()) {
+      alert('Room does not exist');
+      return;
+    }
+    navigate(`/room/${roomCode}`);
+  }
+
   return (
     <div className="flex items-center justify-center h-screen">
       <main className="mx-auto flex flex-col justify-center items-center w-full gap-6">
@@ -35,11 +56,16 @@ export function Home() {
             <div className="h-[0.5px]  bg-gray-400 w-14" />
           </div>
 
-          <form className="flex flex-col items-center justify-center gap-5">
+          <form
+            className="flex flex-col items-center justify-center gap-5"
+            onSubmit={handleJoinRoom}
+          >
             <input
               type="text"
               placeholder="Type the room code"
               className="h-12 border rounded-md p-4 w-96"
+              onChange={(event) => setRoomCode(event.target.value)}
+              value={roomCode}
             />
             <Button>Enter the room</Button>
           </form>
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,4 +1,4 @@
-import { getDatabase, ref, set } from 'firebase/database';
+import { child, getDatabase, push, ref, set } from 'firebase/database';
 import { FormEvent, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
@@ -19,13 +19,17 @@ export function NewRoom() {
       return;
     }
     const db = getDatabase(app);
-    set(ref(db, 'rooms'), {
-      id: user?.id + newRoom,
+    // set ->	Write or replace data to a defined path, like messages/users/<username>
+    // update ->	Update some of the keys for a defined path without replacing all of the data
+    // push ->	Add to a list of data in the database. Every time you push a new node onto a list, your database generates a unique key, like messages/users/<unique-user-id>/<username>
+    // transaction ->	Use transactions when working with complex data that could be corrupted by concurrent updates
+
+    const firebaseRoom = await push(ref(db, 'rooms'), {
       title: newRoom,
       authorId: user?.id,
     });
 
-    navigate(`/rooms/${user?.id + newRoom}`);
+    navigate(`/room/${firebaseRoom.key}`);
   }
   return (
     <div className="flex items-center justify-center h-screen">
